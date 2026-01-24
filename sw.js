0ac6a7012328910c5abd55ed6e2c90bb3f77ef6f
Beast_Beats <ritukumariraj34@gmail.com>
2026-01-24 18:09:01 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "neon-strike-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  // IMPORTANT: handle app launch / reload
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      caches.match("./index.html")
+    );
+    return;
+  }
+
+  // Other assets
+  event.respondWith(
+    caches.match(event.request).then(
+      res => res || fetch(event.request)
+    )
+  );
+});
